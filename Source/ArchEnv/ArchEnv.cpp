ca901e96323229526972f221257d73a9c50c69da
FrancoBarrera99 <56893886+FrancoBarrera99@users.noreply.github.com>
2023-04-04 21:54:44 -0300

    Project

---
+++ Source/ArchEnv/ArchEnv.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArchEnv.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArchEnv, "ArchEnv" );
